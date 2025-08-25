ca1e14c852352e3c793aa182c0532aca9828918b
55155 <qkdtjdwls1024@gmail.com>
2025-08-25 07:44:21 -0400

    add TEST_link2.py

--- src/My_robot/TEST_link2.py
+++ src/My_robot/TEST_link2.py
@@ -1,18 +1,23 @@
 import genesis as gs
 
-gs.init(backend=gs.cuda, logging_level="debug")
+gs.init(backend=gs.cpu, logging_level="debug")
 
 scene = gs.Scene(
+    viewer_options=gs.options.ViewerOptions(
+        # run_in_thread=True,
+    ),
     show_viewer=True,
 )
 plane = scene.add_entity(
     gs.morphs.Plane(),
 )
 scene.add_entity(
-    gs.morphs.Mesh(
-        file='/home/seongjin/Desktop/Seongjin/genesis_simulation_on_linux/My_asset/TEST_LINK_sizeup_description/meshes/base_link.stl',
-        scale=(0.001, 0.001, 0.001),
+    gs.morphs.URDF(
+        file='/home/seongjin/Desktop/Seongjin/genesis_simulation_on_linux/My_asset/TEST_LINK_sizeup_description/urdf/TEST_LINK_sizeup.urdf',
+        scale=0.001,
     ),
 )
 scene.build()
-# scene.viewer.run()
+
+for i in range(100):
+    scene.step()
