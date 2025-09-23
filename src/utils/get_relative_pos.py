d7b2623bd4350a93787591d96d6a38c33c490955
55155 <qkdtjdwls1024@gmail.com>
2025-09-22 23:30:57 -0400

    20250923 add sensor system

--- src/utils/get_relative_pos.py
+++ src/utils/get_relative_pos.py
@@ -1,3 +1,5 @@
+
+'''
 import mujoco
 
 with open('/home/seongjin/Desktop/Seongjin/genesis_simulation_on_linux/My_asset/Crank_slider_system_V3_Pjoint_description/urdf/Crank_slider_system_V3_Pjoint.xml', 'r') as f:
@@ -8,7 +10,7 @@ import os
 stl_file_list = [stl for stl in os.listdir('/home/seongjin/Desktop/Seongjin/genesis_simulation_on_linux/My_asset/Crank_slider_system_V3_Pjoint_description/urdf/') if stl.endswith('.stl')] 
 print(stl_file_list)
 for stl_file in stl_file_list:
-   with open('/home/seongjin/Desktop/Seongjin/genesis_simulation_on_linux/My_asset/Crank_slider_system_V3_Pjoint_description/urdf/'+stl_file, 'rb') as f:
+   with open('/home/seongjin/Desktop/Seongjin/g enesis_simulation_on_linux/My_asset/Crank_slider_system_V3_Pjoint_description/urdf/'+stl_file, 'rb') as f:
        ASSETS[stl_file] = f.read()
 
 
@@ -20,3 +22,14 @@ print(data.)
 # while data.time < 1:
 #   mujoco.mj_step(model, data)
 #   print(data.geom_xpos)
+'''
+
+import numpy as np
+from stl import mesh
+
+your_mesh = mesh.Mesh.from_file('/home/seongjin/Desktop/Seongjin/genesis_simulation_on_linux/My_asset/Crank_slider_system_V3_Pjoint_description/urdf/motor_shaft_1.stl')
+min_corner = np.min(your_mesh.vectors, axis=(0,1))
+max_corner = np.max(your_mesh.vectors, axis=(0,1))
+print("motor shaft 최소 좌표:", min_corner)
+print("motor shaft 최대 좌표:", max_corner)
+    # <body name="motor_shaft_1" pos="0.11 0.05 -0.163">
