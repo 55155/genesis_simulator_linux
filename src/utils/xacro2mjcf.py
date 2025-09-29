88a14abac3f6f2c48fb6c6dd58a2a993f2987774
55155 <qkdtjdwls1024@gmail.com>
2025-09-29 09:29:04 -0400

    20250929 dummy tablet

--- src/utils/xacro2mjcf.py
+++ src/utils/xacro2mjcf.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
         config = yaml.safe_load(file)
 
     asset_path = config["file_path"]["asset"]
-    Robot_name = "Crank_slider_system_V3_Pjoint"
+    Robot_name = "dummy_tablet"
     file_path = asset_path + Robot_name + "_description" + "/urdf/"
     xacro_file = file_path + Robot_name + ".xacro"
     urdf_file = file_path + Robot_name + ".urdf"
